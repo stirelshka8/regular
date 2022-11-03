49594202f41a18a6eaa130e4776abe84b8ed3ca7
stirelshka8 <stirelshka8@gmail.com>
2022-11-03 23:54:23 +0300

    work

--- main.py
+++ main.py
@@ -3,10 +3,29 @@ import csv
 
 
 def read_csv(filename):
+    dict_contact = []
     with open(filename) as read_file:
         rows = csv.reader(read_file, delimiter=",")
         contacts_list = list(rows)
-        pprint(contacts_list)
+        # pprint(contacts_list[0])
+
+        # Переменная хранит в себе имена полей
+        dict_key = contacts_list[0]
+        # Переменная хранит в себе данные таблицы
+        dict_val = contacts_list[1:]
+
+        # Извлекаем данные, попутна нумеруя
+        for data_key, data_val in enumerate(dict_val):
+            dict_contact.append({})
+            # print(data_key)
+            # print(data_val)
+            # Формируем словарь
+            for format_key, format_val in zip(dict_key, data_val):
+                # pprint(format_key)
+                # pprint(format_val)
+                dict_contact[data_key].update({format_key: format_val})
+
+    print(dict_contact[0])
 
 
 # with open("phonebook.csv", "w") as f:
