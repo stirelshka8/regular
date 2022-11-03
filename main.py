94e82ff25eeb37081d8478e71629560d2d91b6e9
stirelshka8 <stirelshka8@gmail.com>
2022-11-03 22:53:17 +0300

    work

--- main.py
+++ main.py
@@ -2,31 +2,16 @@ from pprint import pprint
 import csv
 
 
-with open("phonebook_raw.csv") as f:
-    rows = csv.reader(f, delimiter=",")
-    contacts_list = list(rows)
-    pprint(contacts_list)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+def read_csv(filename):
+    with open(filename) as read_file:
+        rows = csv.reader(read_file, delimiter=",")
+        contacts_list = list(rows)
+        pprint(contacts_list)
 
 
 # with open("phonebook.csv", "w") as f:
 #     datawriter = csv.writer(f, delimiter=',')
 #     datawriter.writerows(contacts_list)
+
+if __name__ == '__main__':
+    read_csv("phonebook_raw.csv")
